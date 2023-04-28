40bd51287d7181d3a08872fef5788b5362679d19
vuho <vu.ho@onemount.com>
2023-04-28 23:33:19 +0700

    feat(mysql_config): add mysql config and parser

--- pkg/config/mysql.go
+++ pkg/config/mysql.go
@@ -1,13 +1,24 @@
 package config
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/kelseyhightower/envconfig"
+)
 
 type MySQL struct {
-	Host     string
-	Port     int
-	Username string
-	Password string
-	Database string
+	Host     string `envconfig:"MYSQL_HOST"`
+	Port     int    `envconfig:"MYSQL_PORT"`
+	Username string `envconfig:"MYSQL_USERNAME"`
+	Password string `envconfig:"MYSQL_PASSWORD"`
+	Database string `envconfig:"MYSQL_DATABASE"`
+}
+
+func NewMySQLConfig() (MySQL, error) {
+	var config MySQL
+	if err := envconfig.Process("", &config); err != nil {
+		return config, err
+	}
+	return config, nil
 }
 
 func (m MySQL) ConnectionString() string {
